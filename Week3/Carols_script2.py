8e12083599a4eb8ea7352a534508a87d15b1f3f5
Carol Molyneaux <carolmcquillan@hotmail.com>
2023-04-10 15:15:16 +0100

    More testing

---
+++ Week3/Carols_script2.py
@@ -0,0 +1,83 @@
+import geopandas as gpd
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from cartopy.feature import ShapelyFeature
+import cartopy.crs as ccrs
+import os
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+
+
+# try to print the results to the screen using the format method demonstrated in the workbook
+plt.ion() # make the plotting interactive
+
+
+myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
+
+# load the necessary data here and transform to a UTM projection
+Counties = gpd.read_file(os.path.abspath('data_files/Counties.shp'))
+Wards = gpd.read_file(os.path.abspath('data_files/NI_Wards.shp'))
+
+for ind, row in Wards.iterrows(): # iterate over each row in the GeoDataFrame
+    Wards.loc[ind, 'Area'] = row['geometry'].area # assign the row's geometry length to a new column, Length
+
+
+# your analysis goes here...
+
+join = gpd.sjoin(Counties, Wards, how='inner', lsuffix='left', rsuffix='right') # perform the spatial join of counties and wards
+#joinedData = join.groupby(['CountyName'])['Population'].sum() # assign summary data to variable to be printed
+#print(joinedData)
+    
+    
+
+Counties = Counties.to_crs(epsg=2157)
+Wards = Wards.to_crs(epsg=2157)
+Wards['Pop_km2'] = Wards['Population']/Wards['Area'] *0.00001
+# your analysis goes here...
+
+join = gpd.sjoin(Counties, Wards, how='inner', lsuffix='left', rsuffix='right') # perform the spatial join of counties and wards
+#joinedData = join.groupby(['CountyName'])['Population'].sum() # assign summary data to variable to be printed
+#print(joinedData)
+
+
+# below here, you may need to modify the script somewhat to create your map.
+# create a crs using ccrs.UTM() that corresponds to our CRS
+myCRS = ccrs.UTM(29)
+# create a figure of size 10x10 (representing the page size in inches
+
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
+
+
+
+# add gridlines below
+gridlines = ax.gridlines(draw_labels=True,
+                         xlocs=[-8, -s7.5, -7, -6.5, -6, -5.5],
+                         ylocs=[54, 54.5, 55, 55.5],
+                         linewidth = 0.5, color = 'gray', alpha = 0.5, linestyle='--')
+
+gridlines.right_labels = False
+gridlines.bottom_labels = False
+gridlines.left_labels = True
+gridlines.top_labels = True
+
+# to make a nice colorbar that stays in line with our map, use these lines:
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
+
+
+# plot the ward data into our axis, using
+ward_plot = Wards.plot(column='Pop_km2', ax=ax, vmin=0.57, vmax=800, cmap='viridis',
+                       legend=True, cax=cax, legend_kwds={'label': 'Population per km2'})
+
+
+county_outlines = ShapelyFeature(Counties['geometry'], myCRS, edgecolor='r', facecolor='none')
+
+
+ax.add_feature(county_outlines)
+county_handles = [mpatches.Rectangle((0, 0), 1, 1, facecolor='none', edgecolor='r')]
+
+ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
+
+# save the figure
+fig.savefig('sample_map2.png', dpi=300, bbox_inches='tight')
