8b1a26e8e93361ada73965fea19ebc7f068fce20
carolmcquillan <carolmcquillan@hotmail.com>
2023-04-25 21:31:40 +0100

    learning

--- Week3/Carols_script2.py
+++ Week3/Carols_script2.py
@@ -15,29 +15,16 @@ plt.ion() # make the plotting interactive
 myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
 
 # load the necessary data here and transform to a UTM projection
-Counties = gpd.read_file(os.path.abspath('data_files/Counties.shp'))
-Wards = gpd.read_file(os.path.abspath('data_files/NI_Wards.shp'))
-
-for ind, row in Wards.iterrows(): # iterate over each row in the GeoDataFrame
-    Wards.loc[ind, 'Area'] = row['geometry'].area # assign the row's geometry length to a new column, Length
-
-
+counties = gpd.read_file(os.path.abspath('c:/Carol_PG_CERT_GIS/egm722_Practicals/egm722/week2/data_files/Counties.shp'))
+wards = gpd.read_file(os.path.abspath('c:/Carol_PG_CERT_GIS/egm722_Practicals/egm722/week3/data_files/NI_Wards.shp'))
+counties.to_crs(epsg=2157)
+wards.to_crs(epsg=2157)
 # your analysis goes here...
 
-join = gpd.sjoin(Counties, Wards, how='inner', lsuffix='left', rsuffix='right') # perform the spatial join of counties and wards
-#joinedData = join.groupby(['CountyName'])['Population'].sum() # assign summary data to variable to be printed
-#print(joinedData)
-    
-    
+join = gpd.sjoin(counties, wards, how='inner', lsuffix='left', rsuffix='right')
 
-Counties = Counties.to_crs(epsg=2157)
-Wards = Wards.to_crs(epsg=2157)
-Wards['Pop_km2'] = Wards['Population']/Wards['Area'] *0.00001
-# your analysis goes here...
-
-join = gpd.sjoin(Counties, Wards, how='inner', lsuffix='left', rsuffix='right') # perform the spatial join of counties and wards
-#joinedData = join.groupby(['CountyName'])['Population'].sum() # assign summary data to variable to be printed
-#print(joinedData)
+joinedData= join.groupby(['CountyName'])['Population'].sum()
+print(joinedData)
 
 
 # below here, you may need to modify the script somewhat to create your map.
@@ -45,14 +32,13 @@ join = gpd.sjoin(Counties, Wards, how='inner', lsuffix='left', rsuffix='right') 
 myCRS = ccrs.UTM(29)
 # create a figure of size 10x10 (representing the page size in inches
 
-
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
 
 
 
 # add gridlines below
 gridlines = ax.gridlines(draw_labels=True,
-                         xlocs=[-8, 7.5, -7, -6.5, -6, -5.5],
+                         xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],
                          ylocs=[54, 54.5, 55, 55.5],
                          linewidth = 0.5, color = 'gray', alpha = 0.5, linestyle='--')
 
@@ -67,11 +53,17 @@ cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
 
 # plot the ward data into our axis, using
-ward_plot = Wards.plot(column='Pop_km2', ax=ax, vmin=0.57, vmax=800, cmap='viridis',
-                       legend=True, cax=cax, legend_kwds={'label': 'Population per km2'})
+ward_plot = Wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
+                       legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
 
 
-county_outlines = ShapelyFeature(Counties['geometry'], myCRS, edgecolor='r', facecolor='none')
+
+
+county_outline = ShapelyFeature(counties['geometry'], # first argument is the geometry
+                            myCRS, # second argument is the CRS
+                            edgecolor='lightblue', # set the edgecolor to be mediumblue
+                            facecolor='lightblue', # set the facecolor to be mediumblue
+                            linewidth=1) # set the outline width to be 1 pt
 
 
 ax.add_feature(county_outlines)
@@ -80,4 +72,6 @@ county_handles = [mpatches.Rectangle((0, 0), 1, 1, facecolor='none', edgecolor='
 ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
 
 # save the figure
-fig.savefig('sample_map2.png', dpi=300, bbox_inches='tight')
+fig.savefig('sample_map22.png', dpi=300, bbox_inches='tight')
+
+
